f0cf34d8b6c519cd3b7ab1c3008b6076d67fad99
afoxbr <antonyrbramwell@gmail.com>
2022-09-01 23:47:53 +0100

    Split modules, reorganise functions

--- src/app.js
+++ src/app.js
@@ -1,20 +1,69 @@
 import { createProject } from './dom';
 
-class toDoCard {
-    constructor(title, description, dueDate, priority, checklist) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
+const projectControl = () => {
+    let projectArray = [];
+
+    class project {
+        constructor(title, toDoArray) {
+            this.title = title;
+            this.toDoArray = toDoArray;
+        };
     };
+
+    const newProjectButton = document.createElement("button");
+    newProjectButton.innerHTML = "new project";
+    newProjectButton.setAttribute("id", "newProjectButton");
+    body.appendChild(newProjectButton);
+
+    const formDiv = document.createElement("div");
+    formDiv.setAttribute("id", "formDiv");
+    document.getElementById("content").appendChild(formDiv);
+
+    newProjectButton.addEventListener("click", () => {
+        
+        let newProjectForm = document.createElement("form");
+        newProjectForm.setAttribute("id", "newProjectForm");
+        formDiv.appendChild(newProjectForm);
+
+        let projectNameInput = document.createElement("input");
+        projectNameInput.setAttribute("type", "text");
+        projectNameInput.setAttribute("name", "projectTitle");
+        projectNameInput.setAttribute("id", "projectNameInput");
+        projectNameInput.setAttribute("placeholder", "Project Title");
+        newProjectForm.appendChild(projectNameInput);
+
+        let newProjectSubmit = document.createElement("button");
+        newProjectSubmit.innerHTML = "Submit";
+        newProjectForm.appendChild(newProjectSubmit);
+        
+        newProjectSubmit.addEventListener("click", () => {
+            let submittedProject = new project()
+            submittedProject.title = document.getElementById("projectNameInput").value;
+            submittedProject.toDoArray = [];
+            projectArray.push(submittedProject);
+            formDiv.innerHTML = '';
+        });
+    });
 };
 
+
 function newToDoButton() { 
-    document.getElementsByClassName("newToDo").addEventListener("click", () => {
     
+    class toDoCard {
+        constructor(title, description, dueDate, priority, checklist) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+        };
+    };
+    
+    document.getElementsByClassName("newToDo").addEventListener("click", () => {
+        
+  
         let newTaskForm = document.createElement("form");
         newTaskForm.setAttribute("id", "newTaskForm");
-        project.appendChild(newTaskForm);
+        formSection.appendChild(newTaskForm);
 
         let titleInput = document.createElement("input");
         titleInput.setAttribute("type", "text");
@@ -41,11 +90,30 @@ function newToDoButton() {
 
         let priorityLabel = document.createElement("label");
         priorityLabel.setAttribute("for", "prioritycheck");
+        priorityLabel.innerHTML = "Tick for high priority";
+        prioritySection.appendChild(priorityLabel);
 
         let priorityInput = document.createElement("input");
         priorityInput.setAttribute("type", "checkbox");
         priorityInput.setAttribute("name", "priorityCheck");
+        priorityInput.setAttribute("id", "toDoPriority");
         prioritySection.appendChild(priorityInput);
+
+        let submitNewTask = document.createElement("button");
+        submitNewTask.innerHTML = "Submit";
+        newTaskForm.appendChild(submitNewTask);
+        submitNewTask.addEventListener("click", () => {
+            let submittedToDoCard = new toDoCard()
+            submittedToDoCard.title = document.getElementById("toDoTitle").value;
+            submittedToDoCard.description = document.getElementById("toDoDesc").value;
+            submittedToDoCard.dueDate = document.getElementById("toDoDate").value;
+            submittedToDoCard.priority = document.getElementById("toDoPriority").ariaChecked;
+
+            toDoArray.push(submittedToDoCard);
+        })
 })};
 
-export { newToDoButton };
+export { 
+    projectControl,
+    newToDoButton,
+};
--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,33 @@
+import { projectControl } from './app';
+
 function createProject() {
-    const project = document.createElement("div");
-    project.setAttribute("class", "project");
-    document.getElementById("content").appendChild(project);
-    const projectTitle = document.createElement("h2");
-    projectTitle.innerHTML = "Daily Tasks";
-    project.appendChild(projectTitle);
-    const toDoCards = document.createElement("div");
-    project.appendChild(toDoCards);
-    const toDoButton = document.createElement("button");
-    toDoButton.setAttribute("class", "newToDo");
-    toDoButton.innerHTML = "create task";
-    project.appendChild(toDoButton);
+    for (i = 0; i < projectArray.length; i++) {
+        let project = document.createElement("div");
+        project.setAttribute("class", "project");
+        document.getElementById("content").appendChild(project);
+        
+        let projectTitle = document.createElement("h2");
+        projectTitle.innerHTML = projectArrauy[i].title;
+        project.appendChild(projectTitle);
+        
+        let toDoCards = document.createElement("div");
+        project.appendChild(toDoCards);
+
+        if (projectArray[i].toDoArray.length > 0) {
+            for (i = 0; i < projectArray[i].toDoArray.length; i ++){
+                //write dom manipulation stuff for to do cards here
+            };
+        };
+
+        let formSection = document.createElement("div");
+        formSection.setAttribute("id", "formSection");
+        project.appendChild(formSection);
+        
+        let toDoButton = document.createElement("button");
+        toDoButton.setAttribute("class", "newToDo");
+        toDoButton.innerHTML = "create task";
+        project.appendChild(toDoButton);
+    };        
 };
 
 export { createProject };
