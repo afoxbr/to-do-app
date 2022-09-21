e5f3f44bea3443b335a3b609651798114abf5620
afoxbr <antonyrbramwell@gmail.com>
2022-09-21 19:58:19 +0100

    Linting

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (localStorage.getItem("projectArrayStorage")) {
       appObject.projectArray[q].toDoArray = JSON.parse(localStorage.getItem(`taskArrayStorage[${q}]`));
       for (let y = 0; y < appObject.projectArray[q].toDoArray.length; y++){
           appObject.projectArray[q].toDoArray[y] = JSON.parse(localStorage.getItem(`taskDetailsStorage[${q+y}]`));
-      };
-  };
-};
+      }
+  }
+}
 
 createProject();
